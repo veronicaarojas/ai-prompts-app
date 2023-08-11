18c0fc0ae46301bd0832ea6b10573a7a041fb9a3
Veronica Rojas <ar_veronica@yahoo.com>
2023-08-11 13:42:31 +0100

    homepage styles and component files

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import '@styles/globals.css'
+import { Children } from 'react'
+
+export const metadata = {
+  title: "Abundance of Prompts",
+  description: "Discover and share AI prompts"
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className='main'>
+        <div className="gradient"/>
+        </div>
+        
+        <main className='app'>
+          {children}
+        </main>
+
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout;
---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+ 
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex flex-col">
+    <h1 className="head_text text-center">
+        Discover & Share
+      <br className="max-md:hidden"/>
+      <span className="orange_gradient text-center">
+        Prompts to power your AI ChatBot
+      </span>
+    </h1>
+    <p className="desc text-center">
+      An open-source 
+      prompting tool for modern world users to discover and share prompts, with the goal of
+      maximise the power of ever-evolving AI.
+    </p>
+
+    {/* Feed */}
+
+    </section>
+  )
+}
+
+export default Home;
