bd989ca01f39d2ec451d7216e16863c4a8c916fd
Veronica Rojas <ar_veronica@yahoo.com>
2023-08-16 11:31:19 +0100

    dynamic profile page

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,70 @@
+'use client'
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Profile from '@components/profile';
+
+
+ 
+ 
+
+function ProfilePage() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      if(session?.user.id) {
+        setPosts(data);
+      }
+      }
+
+    fetchPosts();
+
+  }, [posts])
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
+    
+  }
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm('Are you sure you want to delete this prompt?');
+
+    if(hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        });
+        const filteredPosts = posts.filter((p) => p._id === post._id);
+
+        setPosts(filteredPosts);
+      } catch(error) {
+
+      }
+    }
+  }
+
+  return (
+    <div>
+
+    
+    <Profile
+    name="My"
+    desc="Welcome to your profile"
+    data={posts}
+    handleEdit={handleEdit}
+    handleDelete={handleDelete}/>
+    <div>
+    </div>
+    </div>
+  )
+}
+
+export default ProfilePage
